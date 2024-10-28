b33303dcf0d1b925341703e30af6646009aa620c
Bhekani Khumalo <bhekanik@gmail.com>
2024-10-28 22:50:14 +0000

    Add CV optimiser link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,30 @@
 import { config } from "@/lib/config";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="border-t dark:border-gray-700 pb-[calc(5em+env(safe-area-inset-bottom))] md:pb-0">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-[calc(1em+env(safe-area-inset-bottom))]">
-        <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
-          <div className="flex space-x-4">
+    <footer className="max-w-7xl w-full py-8 mx-auto px-4 sm:px-6 lg:px-8 border-t dark:border-gray-700">
+      <div className="flex w-full items-start justify-between py-8">
+        <Link
+          href="/"
+          className={cn(
+            "font-oswald flex gap-2 flex-col md:flex-row items-start text-2xl font-bold"
+          )}
+        >
+          <Image
+            src="/logo.png"
+            alt={`${config.projectName} Logo`}
+            width={60}
+            height={60}
+          />
+          <span className="hidden md:block">{config.projectName}</span>
+        </Link>
+
+        <div className="flex justify-between items-start gap-8">
+          <div className="flex flex-col space-y-2">
+            <h3 className="text-sm font-medium">Legal</h3>
             <Link
               href="/privacy-policy"
               className="text-sm text-gray-600 dark:text-gray-300 hover:underline"
@@ -20,11 +38,28 @@ export function Footer() {
               Terms of Service
             </Link>
           </div>
-          <p className="text-sm text-gray-600 dark:text-gray-300">
-            © 2024 {config.projectName}. All rights reserved.
-          </p>
+          <div className="flex flex-col space-y-2">
+            <h3 className="text-sm font-medium">More from the creator</h3>
+            <Link
+              href="https://www.cvoptimiser.com/"
+              target="_blank"
+              className="text-sm text-gray-600 dark:text-gray-300 hover:underline"
+            >
+              CV Optimiser
+            </Link>
+            <Link
+              href="https://dealbase.africa/"
+              target="_blank"
+              className="text-sm text-gray-600 dark:text-gray-300 hover:underline"
+            >
+              Dealbase Africa
+            </Link>
+          </div>
         </div>
       </div>
+      <p className="text-sm text-gray-600 dark:text-gray-300 text-center">
+        © 2024 CV Optimiser. All rights reserved.
+      </p>
     </footer>
   );
 }
