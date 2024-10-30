2f380e81060175ff668bb6bf68c428d83e4fb7ee
Bhekani Khumalo <bhekanik@gmail.com>
2024-10-30 10:54:07 +0000

    Some copy improvements on the hero

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -3,16 +3,26 @@
 import { Button } from "@/components/ui/button";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
-    <Button
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      variant="ghost"
-      size="icon"
-    >
+    <Button onClick={toggleTheme} variant="ghost" size="icon">
       <Sun className="hidden dark:block h-[1.2rem] w-[1.2rem] rotate-90 transition-all duration-300 dark:-rotate-0" />
       <Moon className="dark:hidden block h-[1.2rem] w-[1.2rem] rotate-0 transition-all duration-300 dark:rotate-90" />
       <span className="sr-only">Toggle theme</span>
