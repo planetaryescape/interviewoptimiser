3985d1224b62b996e811e492e737826c991028b8
Bhekani Khumalo <bhekanik@gmail.com>
2024-11-08 01:52:04 +0000

    Add some caching

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -70,6 +70,7 @@ resource "aws_iam_policy" "lambda_s3_write_policy" {
           "s3:ListObjectsV2"
         ],
         Resource = [
+          aws_s3_bucket.lambda_bucket.arn,
           "${aws_s3_bucket.lambda_bucket.arn}/*",
           "${aws_s3_bucket.lambda_bucket.arn}/pdfs/*",
           "${aws_s3_bucket.lambda_bucket.arn}/backups/database/*",
