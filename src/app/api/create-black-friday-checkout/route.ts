e3c205d32b08e906d7dbbcb30d9a21b7cbdb9930
Bhekani Khumalo <bhekanik@gmail.com>
2024-11-26 20:44:51 +0000

    Add black friday promo

---
+++ src/app/api/create-black-friday-checkout/route.ts
@@ -0,0 +1,74 @@
+import { getUserFromClerkId } from "@/lib/auth";
+import { config } from "@/lib/config";
+import { logger } from "@/lib/logger";
+import { formatErrorEntity } from "@/lib/utils/formatEntity";
+import { getAuth } from "@clerk/nextjs/server";
+import * as Sentry from "@sentry/nextjs";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2024-10-28.acacia",
+});
+
+export async function POST(request: NextRequest) {
+  try {
+    const { userId: clerkUserId } = getAuth(request);
+    if (!clerkUserId) {
+      logger.error({ clerkUserId }, "Unauthorized");
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const { id: userId } = await getUserFromClerkId(clerkUserId);
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const { stripeCustomerId } = await getUserFromClerkId(clerkUserId);
+
+    const session = await stripe.checkout.sessions.create({
+      customer: stripeCustomerId,
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          quantity: 1,
+          price: config.blackFridayPriceId,
+          adjustable_quantity: {
+            enabled: true,
+          },
+        },
+      ],
+      mode: "payment",
+      automatic_tax: { enabled: true },
+      customer_update: {
+        address: "auto",
+      },
+      success_url: `${config.baseUrl}/checkout/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${config.baseUrl}/checkout/black-friday`,
+      metadata: {
+        userId,
+        productType: "minutes",
+        minutesAmount: 500,
+        promoType: "black-friday-2024",
+      },
+    });
+
+    return NextResponse.json({ sessionId: session.id, url: session.url });
+  } catch (error) {
+    Sentry.withScope((scope) => {
+      scope.setExtra("context", "POST /api/create-black-friday-checkout");
+      scope.setExtra("error", error);
+      Sentry.captureException(error);
+    });
+    logger.error(
+      {
+        message: error instanceof Error ? error.message : "Unknown error",
+        error,
+      },
+      "Error creating checkout session"
+    );
+    return NextResponse.json(formatErrorEntity("Internal server error"), {
+      status: 500,
+    });
+  }
+}
