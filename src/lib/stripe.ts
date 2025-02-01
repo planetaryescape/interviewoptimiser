652ed85040d50d438005527b4cda2e3029c38d9f
Bhekani Khumalo <bhekanik@gmail.com>
2025-02-01 19:59:17 +0000

    Replace pnpm with bun

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,6 +3,6 @@ import Stripe from "stripe";
 export const stripe = new Stripe(
   process.env.STRIPE_SECRET_KEY ?? "sk_test_JA==",
   {
-    apiVersion: "2024-10-28.acacia",
+    apiVersion: "2025-01-27.acacia",
   }
 );
